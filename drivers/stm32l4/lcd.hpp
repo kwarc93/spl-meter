ad3170f5a1553261158bd96f875e075d037b8d5c
Kamil Worek <kamil.worek@protonmail.com>
2021-06-26 18:02:38 +0200

    Added LCD driver class.

---
+++ drivers/stm32l4/lcd.hpp
@@ -0,0 +1,24 @@
+/*
+ * lcd.hpp
+ *
+ *  Created on: 26 cze 2021
+ *      Author: kwarc
+ */
+
+#ifndef STM32L4_LCD_HPP_
+#define STM32L4_LCD_HPP_
+
+namespace drivers
+{
+
+class lcd final
+{
+public:
+    lcd() = delete;
+
+
+};
+
+}
+
+#endif /* STM32L4_LCD_HPP_ */
