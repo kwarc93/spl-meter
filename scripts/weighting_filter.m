3f505e798cbb73b12f4ffbf7b5465950de7ee991
kwarc <kamil.worek@protonmail.com>
2024-02-09 23:07:00 +0100

    Update MATLAB/Octave script

--- scripts/weighting_filter.m
+++ scripts/weighting_filter.m
@@ -1,12 +1,13 @@
-# Script converts analog filter transfer function to digital domain
-# and calculates coefficients for biquad cascade IIR digital filter.
+% Script converts analog filter transfer function to digital domain
+% and calculates coefficients for biquad cascade IIR digital filter.
 
 clear all
 close all
 
 Fs = 40000;
+T = 1/Fs;
 
-# Definition of analog A-weighting filter according to IEC/CD 1672.
+% Definition of analog A-weighting filter according to IEC/CD 1672.
 f1 = 20.598997; 
 f2 = 107.65265;
 f3 = 737.86223;
@@ -17,7 +18,7 @@ num = [(2*pi*f4)^2*(10^(a1000/20)) 0 0 0 0];
 den = conv([1 +4*pi*f4 (2*pi*f4)^2],[1 +4*pi*f1 (2*pi*f1)^2]); 
 den = conv(conv(den,[1 2*pi*f3]),[1 2*pi*f2]);
 
-# Definition of analog C-weighting filter according to IEC/CD 1672.
+% Definition of analog C-weighting filter according to IEC/CD 1672.
 %f1 = 20.598997; 
 %f4 = 12194.217;
 %c1000 = 0.0619;
@@ -25,21 +26,21 @@ den = conv(conv(den,[1 2*pi*f3]),[1 2*pi*f2]);
 %num = [(2*pi*f4)^2*(10^(c1000/20)) 0 0];
 %den = conv([1 +4*pi*f4 (2*pi*f4)^2],[1 +4*pi*f1 (2*pi*f1)^2]); 
 
-# Use bilinear to transform it to a discrete-time IIR filter.
-[numz,denz] = bilinear(num,den,1/Fs);
+% Use bilinear to transform it to a discrete-time IIR filter.
+[numz,denz] = bilinear(num,den, T);
 
-# Visualize filters frequency response
+% Visualize filters frequency response
 figure
 fmin = 10;
 fmax = Fs + 0.1*Fs;
 omega = logspace(log10(2*pi*fmin), log10(2*pi*fmax));
 
-# Analog filter
+% Analog filter
 hs = freqs(num,den,omega);
 semilogx(omega/(2*pi), 20*log10(abs(hs)), 'color', 'b', 'linewidth', 2);
 grid on; hold on;
 
-# Digital filter
+% Digital filter
 n = ceil(Fs/fmin);
 [hz,wz] = freqz(numz,denz,n);
 semilogx(wz*Fs/(2*pi), 20*log10(abs(hz)), 'color', 'r', 'linewidth', 2, 'linestyle', '--');
@@ -49,6 +50,15 @@ legend('analog', 'digital')
 xlabel('Frequency (Hz)')
 ylabel('Magnitude (dB)')
 
-# Get digital filter coefficients (for biquad cascade IIR)
-sos = tf2sos(numz, denz)
+% Get digital filter coefficients (for biquad cascade IIR)
+sos = tf2sos(numz, denz);
+
+% Format coefficients to be compatibile with CMSIS IIR filter functions
+num_sos = size(sos, 1);
+coeffs = zeros(num_sos, 5);
+for s=1:num_sos
+coeffs(s, :) = [sos(s, 1:3) sos(s, 5:6)];
+coeffs(s, 4:5) = -coeffs(s, 4:5);
+endfor
+coeffs
 
